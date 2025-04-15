0b5f8b99e18b0ba1a48bbe1a58480733fb90cb53
sumayyya2214 <sumayya2214@gmail.com>
2025-04-15 18:37:26 -0400

    Initial commit: Upload EmployeeManagement project with full folder structure

---
+++ Context/AppDbContext.cs
@@ -0,0 +1,17 @@
+﻿using EmployeeManagement.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EmployeeManagement.Context
+{
+    public class AppDbContext :DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext>contextOptions)
+            : base(contextOptions)
+        {
+            
+        }
+
+        //
+        public DbSet<Employee>Employees { get; set; }
+    }
+}
